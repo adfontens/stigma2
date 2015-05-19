c3eb9b0cd20a2e3b797228940c401e16650cf901
sh.lee <sh.lee@zespro.co.kr>
2015-05-19 14:32:19 +0900

    modify width, height of chart div

--- public/resources/partials/status.chart_rpm_directive.php
+++ public/resources/partials/status.chart_rpm_directive.php
@@ -1,3 +1,3 @@
-<div style="width: 600px; height: 400px; margin: 0 auto">
+<div style="width: 300px; height: 200px; margin: 0 auto">
 	<div id="container-rpm" style="width: 300px; height: 200px; float: left"></div>
 </div>
